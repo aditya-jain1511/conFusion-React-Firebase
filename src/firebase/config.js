b34236bb893c86555a0ca2f24a3f4d91a9a71cd3
Aditya Jain <aditya151101@gmail.com>
2022-02-14 16:16:07 +0530

    firebase react

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: "AIzaSyAlmXk4pY-BeOuVVcUT1h2ErjMpqWEHotk",
+  authDomain: "confusionserver-test.firebaseapp.com",
+  projectId: "confusionserver-test",
+  storageBucket: "confusionserver-test.appspot.com",
+  messagingSenderId: "182842210642",
+  appId: "1:182842210642:web:a7f9cf5a436228589d628b",
+  measurementId: "G-4LC7NTMER6"
+
+};
